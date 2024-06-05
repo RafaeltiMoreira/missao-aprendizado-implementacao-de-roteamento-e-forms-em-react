dff3f6f1de7fc0ee224c3345848c06ad0b9c200e
Rafael Moreira <52933778+RafaeltiMoreira@users.noreply.github.com>
2024-06-05 02:36:58 -0300

    Criação do `Form`

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,5 +1,30 @@
 export default function Create() {
-    return (
-        <div>Create</div>
-    )
+  return (
+    <div>
+      <h1>Criar DevMon</h1>
+      <div className="flex">
+        <form>
+          <div>
+            <label htmlFor="nome">Nome:</label>
+            <input type="text" id="nome" name="nome" placeholder="Insira o nome" required />
+          </div>
+
+          <div>
+            <label htmlFor="imagem">Imagem (URL):</label>
+            <input type="text" id="imagem" name="imagem" placeholder="Insira a URL da imagem" required />
+          </div>
+
+          <div>
+            <label htmlFor="evoluiPara">Evolui Para (opcional):</label>
+            <input type="text" id="evoluiPara" name="evoluiPara" placeholder="Insira o nome da evolução" />
+          </div>
+
+          <div className="btn">
+            <button type="submit">Criar</button>
+            <button type="reset">Reiniciar</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
