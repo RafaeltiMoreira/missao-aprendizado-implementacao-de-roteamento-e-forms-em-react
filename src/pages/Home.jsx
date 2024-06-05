db63748aea6668da9b9cfbbe1cdfc564960aa85e
Rafael Moreira <52933778+RafaeltiMoreira@users.noreply.github.com>
2024-06-05 03:45:43 -0300

    Import ReactToastify.css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import { Api } from '../api/api'
 import Card from '../components/Card/Card'
 
